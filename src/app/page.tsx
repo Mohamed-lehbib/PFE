9a1a9e11d0c1a3c4ea4d7458a6f5a8184aa8a53c
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-05-23 10:22:56 +0000

    testing the pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home Page
+      <p className="font-700">home Page</p>
       <span className="bg-green-400 text-white hover:bg-white hover:text-green-400 ">
         hello world
       </span>
