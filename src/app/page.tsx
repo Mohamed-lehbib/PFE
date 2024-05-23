dd69dd9d88689efb00892de2243ee4b44af7f7a4
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-05-23 16:35:53 +0000

    fixing sonar issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home Page
+    <main>
+      <p className="text-black first-letter:font-bold">home Page</p>
       <span className="bg-green-400 text-white hover:bg-white hover:text-green-400 ">
         hello world
       </span>
