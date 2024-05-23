61457fd9b55d089ed8304f2397616b595d6cde4d
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-05-23 10:14:05 +0000

    testing the pipeline if it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       home Page
+      <span className="bg-green-400 text-white hover:bg-white hover:text-green-400 ">
+        hello world
+      </span>
     </main>
   );
 }
