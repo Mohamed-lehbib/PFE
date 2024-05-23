35aa4c080dbd07b2ed4abdceb4a7d2c37e31ea60
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-05-23 13:14:32 +0000

    using 21 node version checking if the pipeline would work as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="font-700">home Page</p>
+      home Page
       <span className="bg-green-400 text-white hover:bg-white hover:text-green-400 ">
         hello world
       </span>
