aa70d57534fa1e1f7539ae42be5e9051e2e660f2
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-06-04 15:18:22 +0000

    adding the home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,159 @@
-export default function Home() {
+"use client";
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  LogoutOutlined,
+  ProjectOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import {
+  Breadcrumb,
+  Button,
+  Layout,
+  Menu,
+  theme,
+  message as messageApi,
+} from "antd";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+
+const { Header, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    console.log("Logout function called");
+    try {
+      const supabase = createClient();
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        console.log("Supabase update password error:", error);
+        messageApi.error({
+          content: `Failed to logout: ${error.message}`,
+          duration: 10,
+          className: "custom-class",
+        });
+      } else {
+        messageApi.success({
+          content: `Logout successfully`,
+          duration: 2,
+          className: "custom-class",
+        });
+        router.push("/signin");
+      }
+    } catch (error) {
+      console.error(`Error logout : ${error}`);
+      messageApi.error({
+        content: `Failed to logout: ${error}`,
+        duration: 10,
+        className: "custom-class",
+      });
+    }
+  };
+
+  const menuItems = [
+    {
+      key: "1",
+      icon: <ProjectOutlined />,
+      label: "Projects",
+    },
+    {
+      key: "2",
+      icon: <VideoCameraOutlined />,
+      label: "nav 2",
+    },
+  ];
+
   return (
-    <main>
-      <p className="text-black first-letter:font-bold">home Page</p>
-      <span className="bg-green-400 text-white hover:bg-white hover:text-green-400 ">
-        hello world
-      </span>
-    </main>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{ height: "100vh", backgroundColor: "#2563EB" }} // blue-600
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="light" // Change theme to light to avoid dark default background
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={menuItems}
+          style={{
+            height: "calc(100% - 64px)",
+            borderRight: 0,
+            backgroundColor: "#2563EB",
+          }} // adjust menu height and background color
+        />
+        <div
+          style={{
+            padding: "10px 20px",
+            position: "absolute",
+            bottom: 0,
+            width: "100%",
+          }}
+        >
+          <Button
+            type="primary"
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+            style={{
+              width: "100%",
+            }}
+          >
+            {!collapsed && "Logout"}
+          </Button>
+        </div>
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+            width: "100%",
+            padding: 0,
+            background: colorBgContainer,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content style={{ padding: "0 48px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 380,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Content
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
-}
+};
+
+export default App;
