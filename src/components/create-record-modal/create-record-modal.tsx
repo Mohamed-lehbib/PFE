21ca4607e635ddc39c53471b3fd4fbfcf4788180
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-07-04 13:24:15 +0000

    cleaning the code of the create modal

--- src/components/create-record-modal/create-record-modal.tsx
+++ src/components/create-record-modal/create-record-modal.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Modal, Form, Input, Select, Button, Spin, Upload } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
-
-const { Option } = Select;
+import { Modal, Form, Button, Spin } from "antd";
+import FormFieldsRenderer from "../form-fields-renderer/form-fields-renderer";
 
 interface CreateRecordModalProps {
   visible: boolean;
@@ -26,107 +24,6 @@ const CreateRecordModal: React.FC<CreateRecordModalProps> = ({
       return e;
     }
     return e?.fileList;
-  };
-
-  const renderFormFields = () => {
-    return attributes.map((attr) => {
-      if (attr.type === "enum" || attr.enumValues) {
-        return (
-          <Form.Item
-            key={attr.name}
-            name={attr.name}
-            label={attr.name}
-            rules={[{ required: true, message: `Please select ${attr.name}` }]}
-          >
-            <Select>
-              {attr.enumValues.map((option: string) => (
-                <Option key={option} value={option}>
-                  {option}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-        );
-      } else if (attr.type === "number") {
-        return (
-          <Form.Item
-            key={attr.name}
-            name={attr.name}
-            label={attr.name}
-            rules={[{ required: true, message: `Please input ${attr.name}` }]}
-          >
-            <Input type="number" />
-          </Form.Item>
-        );
-      } else if (attr.type === "string") {
-        if (attr.metaType === "textarea") {
-          return (
-            <Form.Item
-              key={attr.name}
-              name={attr.name}
-              label={attr.name}
-              rules={[{ required: true, message: `Please input ${attr.name}` }]}
-            >
-              <Input.TextArea />
-            </Form.Item>
-          );
-        } else if (attr.metaType === "image" || attr.metaType === "file") {
-          return (
-            <Form.Item
-              key={attr.name}
-              name={attr.name}
-              label={attr.name}
-              valuePropName="fileList"
-              getValueFromEvent={getValueFromEvent}
-              rules={[
-                { required: true, message: `Please upload ${attr.name}` },
-              ]}
-            >
-              <Upload listType="picture" beforeUpload={() => false}>
-                <Button icon={<UploadOutlined />}>Click to upload</Button>
-              </Upload>
-            </Form.Item>
-          );
-        } else {
-          return (
-            <Form.Item
-              key={attr.name}
-              name={attr.name}
-              label={attr.name}
-              rules={[{ required: true, message: `Please input ${attr.name}` }]}
-            >
-              <Input type="text" />
-            </Form.Item>
-          );
-        }
-      } else if (attr.type === "boolean") {
-        return (
-          <Form.Item
-            key={attr.name}
-            name={attr.name}
-            label={attr.name}
-            valuePropName="checked"
-            rules={[{ required: true, message: `Please select ${attr.name}` }]}
-          >
-            <Select>
-              <Option value={true}>True</Option>
-              <Option value={false}>False</Option>
-            </Select>
-          </Form.Item>
-        );
-      } else {
-        return (
-          <Form.Item
-            key={attr.name}
-            name={attr.name}
-            label={attr.name}
-            rules={[{ required: true, message: `Please input ${attr.name}` }]}
-          >
-            <Input type={attr.type} />
-          </Form.Item>
-        );
-      }
-    });
   };
 
   const handleSubmit = () => {
@@ -161,7 +58,10 @@ const CreateRecordModal: React.FC<CreateRecordModalProps> = ({
     >
       <Spin spinning={isSubmitting}>
         <Form form={form} layout="vertical">
-          {renderFormFields()}
+          <FormFieldsRenderer
+            attributes={attributes}
+            getValueFromEvent={getValueFromEvent}
+          />
         </Form>
       </Spin>
     </Modal>
